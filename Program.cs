da971facff947b77ebae162f80942c00fb169414
Jenny Jimenez <jennnifer.jimenez@gmail.com>
2020-09-27 19:56:48 -0400

    new changes

--- Program.cs
+++ Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Project_3B
 {
-    class Program
+  class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = new int[25]; //created array of 25
+            int i;
+            
+            Console.WriteLine("Array is as follows:");
+            Console.WriteLine("_________________________");
+            Console.WriteLine("Elements of Array:");
+
+            try
+            {
+                for (i=0; i<=25; i++)
+                {
+                    Console.WriteLine("Element - {0}", i); //shows elements of array 
+                }
+            Console.WriteLine("Press any key to exit the program.");
+            Console.ReadKey(true);
+
+            }//end try
+
+            catch //incase of errors
+            {
+                Console.WriteLine("Error has occured, please close application and try again");
+                Console.ReadKey(true);
+            }//end catch
         }
     }
 }
